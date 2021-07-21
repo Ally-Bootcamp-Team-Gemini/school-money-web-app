a2910a0f1aa78a508b1120f756b89dd293a7acdd
Elena Komesu <elena.komesu@gmail.com>
2021-07-21 16:19:28 -0400

    initial header

--- src/header.js
+++ src/header.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import logo from './exchange-logo2.png'
 
-export default function Header() {
+export default function Header(props) {
     return (
-        <div> something </div>
+        <div class="header">
+            <img className="App-logo" src={logo} alt="logo" />
+            <h1>Money Exchange</h1>
+            <div style={{"margin-left":"auto"}}>Exchange Count: {props.count}</div>
+        </div>
     )
 }
