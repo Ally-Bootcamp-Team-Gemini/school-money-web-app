9f86123dfe80f7ad4511619883b9b6fca8fef703
William French <william.ray.french@gmail.com>
2021-07-22 10:59:21 -0400

    resolving more conflicts

--- src/App.js
+++ src/App.js
@@ -10,14 +10,20 @@ function App() {
   const [count, setCounter] = useState(0);
 
   const incrementCounter = () => {
-     setCounter(count+1);
-  } 
+    setCounter(count + 1);
+  }
+  const Teammates = ['Elena', 'Satchel', 'Zach', 'Manish', 'Callen', 'Will'];
+
+  const logo = [{
+    Company: '@Money Exchange',
+    Year: 2021,
+  }];
 
   return (
     <div className="App">
       <Header count={count} />
-      <Body incrementCounter={incrementCounter}/>
-      <Footer />
+      <Body incrementCounter={incrementCounter} />
+      <Footer teammates={Teammates} company={logo} />
     </div>
   );
 }
